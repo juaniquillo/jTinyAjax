b94a8cd09d124ac2a66a0c71708b1508c3773f69
Juaniquillo <juaniquillo@juaniquillo.com>
2011-06-07 19:32:36 -0400

    Optimización y JSLint ;-)

--- js/jTinyAjax.jquery.js
+++ js/jTinyAjax.jquery.js
@@ -11,7 +11,7 @@
  * http://www.opensource.org/licenses/mit-license.php
  */
  
-(function($) {
+(function($){
    
    $.fn.jTinyAjax = function (options) {
    
@@ -56,23 +56,23 @@
       //tipo de data a recibirse. HTML para el comportamiento defecto. Si se usa json se espera un campo llamado "data" el cual debe poseer la informacion que se quiere colocar en el editor web
       ajaxTypeData: 'html',
       //callback que se ejecuta cuando se le da click al elemetno para editarlo
-      onClick: function(message){
+      onClick: function(){
          
       },
       //callback que se ejecuta cuando se le da click al boton de cancelar
-      onCancel: function(message){
+      onCancel: function(){
          
       },
       //callback que se ejecuta cuando se le da click al boton de enviar
-      onSend: function(message){
+      onSend: function(){
          
       },
       //callback que se ejecuta cuando hay un error en la consulta
-      onError: function(message){
+      onError: function(){
         
       },
       //callback que se ejecuta cuando se existe un error con la consulta
-      onSuccess: function(message){
+      onSuccess: function(){
          
       }
    },
@@ -88,48 +88,43 @@
       templateFormEd : '<form action="" class="form_edit_ad"><button class="botons_ui edit_bot_ad" onclick="return false"><span class="ui-button-text"> </span> </button><button class="botons_ui cancelar_bot_ad" onclick="return false"><span class="ui-button-text"> </span></button><input type="hidden", class="editar_hid_imp_ad" value="" /></form>',
       //iniciar plugin
       ini: function(objeto, index){
-         //variables de iniciacion
-         var idActual = objeto.attr('id'),
-            idContainer = internal.idEditConatiner + '_' + index,
-            idElem = internal.idEdit + '_' + index;
-            
-         //verificar si el elemento tiene id
-         if(idActual) {
-            idElem = idActual;
-          }
-            
-         //objeto para la data()
-         var dataEle = {
-               'idContainer': idContainer,
-               'idEle': idElem
-          },
-         //agregar formulario
-          formEd = $(internal.templateFormEd).hide().find('.editar_hid_imp_ad').attr('value', index + 1).end();
+		//variables de iniciacion
+		 var idActual = objeto.attr('id') || internal.idEdit + '_' + index,
+			//id del container
+			idContainer = internal.idEditConatiner + '_' + index,
+            //objeto para la data()
+            dataEle = {
+                  'idContainer': idContainer,
+                  'idEle': idActual
+            },
+            //agregar formulario
+            formEd = $(internal.templateFormEd).hide().find('.editar_hid_imp_ad').attr('value', index + 1).end();
          //append texto a los botones
          formEd.find('.edit_bot_ad .ui-button-text').html(options.buttonTextEdit).end().find('.cancelar_bot_ad .ui-button-text').html(options.buttonTextCancel);
          //agregar id, agregar id al container, agregar container y append formulario
-         objeto.addClass(internal.classEdit).attr('id', idElem).wrap('<div id="'+ idContainer +'" class="container_editor_ad" />').data(dataEle).parent().append(formEd);
-         
+         objeto.addClass(internal.classEdit).attr('id', idActual).wrap('<div id="'+ idContainer +'" class="container_editor_ad" />').data(dataEle).parent().append(formEd);
          //botones UI
          if(options.UIfy){
             internal.uiInit(dataEle.idContainer);
          }
-         
       },
       //activar
       clickEdit: function(objeto){
-         var idElem = objeto.data('idEle'), idContainer = objeto.data('idContainer');
+		//id elemento
+		var idElem = objeto.data('idEle'),
+			//id container
+			idContainer = objeto.data('idContainer');
          //delegate para el click
          $('#' + idContainer).delegate('.editjTinyAjax', 'click', function(){
-            elemnt = $('#' + idElem );
-            var content = elemnt.html();
+            var elemnt = $('#' + idElem ),
+               content = elemnt.html();
             //contenido HTML
             $('#' + idElem ).data({
                'html' : content
             });
             //Opciones de TinyMCE
             defaults.tinymceinit.elements = idElem;
-            //callback cuando el editor termine de cargar
+            //callback cuando el editor termine de cargar (actualmente no se usa)
             //options.tinymceinit.init_instance_callback = function(){};
             //tinyMCE Init (Habilitar tinyMCE)
             tinyMCE.init(options.tinymceinit);
@@ -137,24 +132,24 @@
             $('#' + idContainer + ' .form_edit_ad').show();
             //callback
             options.onClick(content);
-
             return false;
 	});
          
       },
       //consulta ajax
       send: function(objeto){
-         var idElem = objeto.data('idEle'), idContainer = objeto.data('idContainer'), container = $('#' + idContainer);
+         //id elemento
+		 var idElem = objeto.data('idEle'),
+			//id container
+			idContainer = objeto.data('idContainer'),
+			//objeto del container
+			container = $('#' + idContainer);
          
          container.delegate('.edit_bot_ad', 'click', function(){
-            //contenido HTML
-            contentHTML = tinyMCE.get(idElem).getContent()
-            //onSend callback
-            options.onSend(contentHTML);
-            //bloquear elemento
-            if(options.blockUI) internal.blockInt(container);
-            //Parametros extra
-            var opcionesAjax = {
+               //contenido HTML
+            var contentHTML = tinyMCE.get(idElem).getContent(),
+               //Parametros extra
+               opcionesAjax = {
                //URL para el ajax
                url: options.url,
                //tipo ajax
@@ -164,36 +159,50 @@
                //tipo de data
                dataType: options.typeData,
                //si se tiene exito
-               success: function(content, textStatus, XMLHttpRequest){
+               success: function(content){
                   //quitar elementos/igualar contenido al obtenido por ajax
-                  if(options.ajaxTypeData == "json"){
+                  if(options.ajaxTypeData === "json"){
                      internal.remove(idElem, idContainer, content.data);
                   }
                   else{
                      internal.remove(idElem, idContainer, content);
                   }
                   //desbloquear
-                  if(options.blockUI) internal.unblockInt(container);
+                  if(options.blockUI) {
+                     internal.unblockInt(container);
+                  }
                   //callback
                   options.onSuccess(content);
                   
                },
-               error: function(XMLHttpRequest, textStatus, errorThrown){
+               error: function(XMLHttpRequest, textStatus){
                   //desbloquear
-                  if(options.blockUI) internal.unblockInt(container);
+                  if(options.blockUI) {
+                     internal.unblockInt(container);
+                  }
                   ////callback
                   options.onError(textStatus);
                }
+            };
+            //onSend callback
+            options.onSend(contentHTML);
+            //bloquear elemento
+            if(options.blockUI) {
+               internal.blockInt(container);
             }
             // Llamada AJAX
             $.ajax(opcionesAjax);
-         
-         })
+         });
          
       },
       //cancelar
       cancel: function(objeto){
-         var idElem = objeto.data('idEle'), idContainer = objeto.data('idContainer');
+		 //id elemento
+		 var idElem = objeto.data('idEle'),
+			//id container
+			idContainer = objeto.data('idContainer'),
+			//content esta vacio
+			content;
          $('#' + idContainer).delegate('.cancelar_bot_ad', 'click', function(){
             content = $('#' + idElem).data('html');
             internal.remove(idElem, idContainer, content);
@@ -204,7 +213,9 @@
       //remover
       remove: function(idElem, idContainer, content){
          //contenido
-         if(content !== '') tinyMCE.get(idElem).setContent(content);
+         if(content !== '') {
+            tinyMCE.get(idElem).setContent(content);
+         }
          //remover instance de tinyMCE
          tinyMCE.execCommand('mceRemoveControl', false, idElem);
          //ocultar formulario
@@ -219,7 +230,7 @@
       //desbloquear plugin
       unblockInt: function(objeto){
          if($.fn.unblock){
-            objeto.unblock()
+            objeto.unblock();
          }
       },
       //botones jquery UI
@@ -230,7 +241,7 @@
          }
       }
         
-   }
+   };
    //para tener id únicos para los editores y containers
    var $index2 = $('div[id^="'+ internal.idEdit +'"]').length;
    return this.each(function(){
@@ -248,4 +259,4 @@
    });
  };
  
-})(jQuery);
+}(jQuery));
